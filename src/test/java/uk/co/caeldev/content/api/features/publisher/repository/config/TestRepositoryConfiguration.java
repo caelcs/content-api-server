b2309b3b77e45eef951ba8572571cef4c3217c7d
caelwinner <adolfoecs@gmail.com>
2015-06-16 21:14:31 +0100

    #3 : Simplify Test Repository Configuration

--- src/test/java/uk/co/caeldev/content/api/features/publisher/repository/config/TestRepositoryConfiguration.java
+++ src/test/java/uk/co/caeldev/content/api/features/publisher/repository/config/TestRepositoryConfiguration.java
@@ -3,22 +3,14 @@ package uk.co.caeldev.content.api.features.publisher.repository.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import uk.co.caeldev.content.api.config.MongoConfiguration;
-import uk.co.caeldev.content.api.config.MongoSettings;
+import uk.co.caeldev.content.api.config.*;
 import uk.co.caeldev.content.api.features.publisher.config.MongoClientTestConfiguration;
 
 @Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = {"uk.co.caeldev.content.api.config"},
-        includeFilters = {
-                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-                        value = {
-                                MongoConfiguration.class
-                        })
-        })
+@EnableAutoConfiguration(exclude = {MongoClientConfiguration.class, MvcConfiguration.class, MethodSecurityConfiguration.class})
+@ComponentScan(basePackages = {"uk.co.caeldev.content.api.config"})
 @EnableMongoRepositories(basePackages = {"uk.co.caeldev.content.api.features.*.repository"})
 @Import({MongoClientTestConfiguration.class, MongoConfiguration.class, MongoSettings.class})
 public class TestRepositoryConfiguration {
