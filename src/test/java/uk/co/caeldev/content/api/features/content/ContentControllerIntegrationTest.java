8c9b58dc1a4f2fc4d3e3a136f4cb612f0c6bec5c
caelwinner <adolfoecs@gmail.com>
2015-06-20 10:58:41 +0100

    #6 : Adding more testing scenarios for Content Controller

--- src/test/java/uk/co/caeldev/content/api/features/content/ContentControllerIntegrationTest.java
+++ src/test/java/uk/co/caeldev/content/api/features/content/ContentControllerIntegrationTest.java
@@ -3,9 +3,6 @@ package uk.co.caeldev.content.api.features.content;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import org.junit.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import uk.co.caeldev.content.api.features.AbstractControllerIntegrationTest;
 
 import java.util.UUID;
@@ -13,10 +10,14 @@ import java.util.UUID;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.jayway.restassured.RestAssured.given;
 import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static uk.co.caeldev.content.api.builders.UserBuilder.userBuilder;
+import static uk.co.caeldev.content.api.features.content.ContentStatus.UNREAD;
 
 
 public class ContentControllerIntegrationTest extends AbstractControllerIntegrationTest {
@@ -28,24 +29,104 @@ public class ContentControllerIntegrationTest extends AbstractControllerIntegrat
         //Given
         final String publisherUUID = "44d74b78-a235-45bf-9aa5-79b72e1531ad";
         final String accessToken = UUID.randomUUID().toString();
+        final String content = "{}";
 
         final String userJson = objectMapper.writeValueAsString(userBuilder().username("(8/F#@]L0K?<c<>Jx*I|`0TjP79zjU").build());
 
+        //And
         stubFor(get(urlEqualTo("/sso/user"))
                 .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK.value())
+                        .withStatus(OK.value())
                         .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                         .withHeader(AUTHORIZATION, accessToken)
                         .withBody(userJson)));
 
+        //When
+        final ContentResource response = given().port(port).basePath(basePath).log().all()
+                .when()
+                    .header(AUTHORIZATION, format("Bearer %s", accessToken))
+                    .body(content)
+                    .contentType(TEXT_PLAIN_VALUE)
+                    .post(format("/publishers/%s/contents", publisherUUID))
+                .then()
+                    .assertThat()
+                    .statusCode(CREATED.value())
+                    .extract().body().as(ContentResource.class);
+
+        //Then
+        verify(getRequestedFor(urlMatching("/sso/user")));
+        assertThat(response.getContent()).isEqualTo(content);
+        assertThat(response.getContentStatus()).isEqualTo(UNREAD);
+        assertThat(response.getContentUUID()).isNotNull();
+        assertThat(response.getContentUUID()).isNotEmpty();
+        assertThat(response.getCreationDate()).isNotNull();
+    }
+
+    @Test
+    @UsingDataSet(loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void shouldReturnBadRequestStatusWhenContentIsEmpty() throws Exception {
+
+        //Given
+        final String publisherUUID = "44d74b78-a235-45bf-9aa5-79b72e1531ad";
+        final String accessToken = UUID.randomUUID().toString();
+        final String content = "";
+
+        final String userJson = objectMapper.writeValueAsString(userBuilder().username("(8/F#@]L0K?<c<>Jx*I|`0TjP79zjU").build());
+
+        //And
+        stubFor(get(urlEqualTo("/sso/user"))
+                .willReturn(aResponse()
+                        .withStatus(OK.value())
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                        .withHeader(AUTHORIZATION, accessToken)
+                        .withBody(userJson)));
+
+        //When
         given().port(port).basePath(basePath).log().all()
                 .when()
-                .header(HttpHeaders.AUTHORIZATION, format("Bearer %s", accessToken))
-                .body("{}")
-                .contentType(MediaType.TEXT_PLAIN_VALUE)
+                    .header(AUTHORIZATION, format("Bearer %s", accessToken))
+                    .body(content)
+                    .contentType(TEXT_PLAIN_VALUE)
+                    .post(format("/publishers/%s/contents", publisherUUID))
+                .then()
+                    .assertThat()
+                    .statusCode(BAD_REQUEST.value());
+
+        //Then
+        verify(getRequestedFor(urlMatching("/sso/user")));
+    }
+
+    @Test
+    @UsingDataSet(loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    public void shouldReturnForbiddenStatusWhenPublisherUUIDAnnUsernameDoNotCorrespond() throws Exception {
+
+        //Given
+        final String publisherUUID = "44d74b78-a235-45bf-9aa5-79b72e1531ad";
+        final String accessToken = UUID.randomUUID().toString();
+        final String content = "{}";
+
+        final String userJson = objectMapper.writeValueAsString(userBuilder().username("test1").build());
+
+        //And
+        stubFor(get(urlEqualTo("/sso/user"))
+                .willReturn(aResponse()
+                        .withStatus(OK.value())
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                        .withHeader(AUTHORIZATION, accessToken)
+                        .withBody(userJson)));
+
+        //When
+        given().port(port).basePath(basePath).log().all()
+                .when()
+                .header(AUTHORIZATION, format("Bearer %s", accessToken))
+                .body(content)
+                .contentType(TEXT_PLAIN_VALUE)
                 .post(format("/publishers/%s/contents", publisherUUID))
                 .then()
                 .assertThat()
-                .statusCode(HttpStatus.CREATED.value());
+                .statusCode(FORBIDDEN.value());
+
+        //Then
+        verify(getRequestedFor(urlMatching("/sso/user")));
     }
 }
