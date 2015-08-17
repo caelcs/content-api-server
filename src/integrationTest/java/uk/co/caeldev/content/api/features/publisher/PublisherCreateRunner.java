8237b290582b1247df56256872d008217573f394
caelwinner <adolfoecs@gmail.com>
2015-08-17 19:51:22 +0100

    #12: Fixing Glue package

--- src/integrationTest/java/uk/co/caeldev/content/api/features/publisher/PublisherCreateRunner.java
+++ src/integrationTest/java/uk/co/caeldev/content/api/features/publisher/PublisherCreateRunner.java
@@ -10,6 +10,6 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber/content/cucumber.json",
                 "junit:target/cucumber/content/cucumber.xml"},
         features = "src/integrationTest/resources/cucumber/uk/co/caeldev/content/api/features/publisher",
-        glue = {"uk.co.caeldev.content.api.features.publisher.create"})
+        glue = {"uk.co.caeldev.content.api.features.publisher"})
 public class PublisherCreateRunner {
 }
